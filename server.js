47b41030c44a2df171cd9c2b71af3887ed36742d
Ash Rotman <quigibow@gmail.com>
2017-10-14 00:04:57 -0600

    added index

--- server.js
+++ server.js
@@ -12,11 +12,67 @@ app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "view.html"));
 });
 
-app.get("/add", function(req, res) {
-  res.sendFile(path.join(__dirname, "add.html"));
+app.get("/tables", function(req, res) {
+  res.sendFile(path.join(__dirname, "tables.html"));
 });
 
 // Get all characters
-app.get("/all", function(req, res) {
-  res.json(characters);
+app.get("/reserve", function(req, res) {
+  res.sendFile(path.join(__dirname, "reserve.html"));
 }); 
+
+const tables = [
+{
+	name: "Ash",
+	phone: "3035555555",
+	email: "ashemail@email.com"
+	customerID: 1
+}
+
+];
+
+app.get('/api/tables', function (req, res) {
+  res.json(tables);
+});
+
+app.post('/api/reserve', function (req, res) {
+	console.log('reservation submitted');
+	console.log(req.body);
+
+  var newReservation = req.body;
+
+  tables.push(newReservation);
+
+  // Check if user is in the first 5 in list
+  var isBooked;
+  if(tables.length <= 5){
+    isBooked = true;
+  }
+  else{
+    isBooked = false;
+  }
+
+  res.json(isBooked);
+
+});
+
+
+app.post('/api/clear', function (req, res) {
+  console.log('clear all tables');
+  tables = [];
+  res.sendFile(path.join(__dirname, 'tables.html'));
+});
+
+app.post('/api/killreservation', function (req, res) {
+  console.log(req.body.id);
+
+  tables.splice(req.body.id, 1);
+  res.json(tables);
+});
+
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function () {
+	console.log('App listening on PORT ' + PORT);
+});
